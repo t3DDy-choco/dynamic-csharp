7f0c79ff78ac9f087eb08603b76b6fc6c6f84334
Talha Salman <talha.salman22@gmail.com>
2021-06-10 20:08:01 +0500

    Added OLD fibonacci
    Created a fibonacci number getter function using standard programming techniques.

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -6,7 +6,14 @@ namespace fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Getting Factorial with old standard method
+            Console.WriteLine(old_fib(5));
+        }
+
+        static int old_fib(int n)
+        {
+            if (n <= 2) return 1;
+            return old_fib(n - 1) + old_fib(n - 2);
         }
     }
 }
