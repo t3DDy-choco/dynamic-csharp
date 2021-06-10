07a906b349cc6b64fce782e7923f9b2b39e3d0e7
Talha Salman <talha.salman22@gmail.com>
2021-06-10 22:42:01 +0500

    Added Memoized Fibonacci
    Added a more efficient Fibonacci Function that can solve very complex calculations in O(n) time.

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -1,19 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace fibonacci
 {
     class Program
     {
+        static Dictionary<int, long> memo = new Dictionary<int, long>() {
+            {0, 0},
+            {1, 1},
+            {2, 1}
+        };
         static void Main(string[] args)
         {
             // Getting Factorial with old standard method
-            Console.WriteLine(old_fib(5));
+            // Console.WriteLine(old_fib(45));
+            Console.WriteLine(memo_fib(450, memo));
         }
 
         static int old_fib(int n)
         {
             if (n <= 2) return 1;
             return old_fib(n - 1) + old_fib(n - 2);
+        }
+
+        static long memo_fib(int n, Dictionary<int, long> memo)
+        {
+            if (memo.ContainsKey(n)) return memo[n];
+            else memo.Add(n, memo_fib(n - 1, memo) + memo_fib(n - 2, memo));
+            return memo[n];
         }
     }
 }
