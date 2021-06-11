51bbd010b3120a88e4eeaf223379433402ed6fe7
Talha Salman <talha.salman22@gmail.com>
2021-06-11 08:27:24 +0500

    Created gridTraveller
    Created a basic gridTraveller program that is basically a modification of the Fibonacci program and uses the same tree principle.

---
+++ gridTraveller/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace gridTraveller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(travel(0, 1));
+            Console.WriteLine(travel(1, 1));
+            Console.WriteLine(travel(3, 3));
+            Console.WriteLine(travel(100, 100));
+        }
+
+        static int travel(int rows, int columns)
+        {
+            if (rows == 1 && columns == 1) return 1;
+            if (rows == 0 || columns == 0) return 0;
+            return travel(rows - 1, columns) + travel(rows, columns - 1);
+        }
+    }
+}
