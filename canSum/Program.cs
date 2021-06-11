b11d3e359fa76f38708bfe41cab52cf25a629e8c
Talha Salman <talha.salman22@gmail.com>
2021-06-11 19:59:17 +0500

    Added howSum
    Added both the brute force and the memoized versions of the howSum function which basically finds out how the target sum can be achieved from a given set of numbers.

--- canSum/Program.cs
+++ canSum/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace canSum
 {
@@ -9,6 +10,10 @@ namespace canSum
             {0, true}
         };
 
+        static Dictionary<int, ArrayList> _memo2 = new Dictionary<int, ArrayList>() {
+            {0, new ArrayList()}
+        };
+
         static void Main(string[] args)
         {
             int[] a1 = { 2, 3 };
@@ -16,11 +21,19 @@ namespace canSum
             int[] a3 = { 2, 4 };
             int[] a4 = { 2, 3, 5 };
             int[] a5 = { 7, 14 };
-            Console.WriteLine(canSum(7, a1));
-            Console.WriteLine(canSum(7, a2));
-            Console.WriteLine(canSum(7, a3));
-            Console.WriteLine(canSum(8, a4));
-            Console.WriteLine(canSum_memo(300, a5, _memo));
+            Console.Write(canSum(7, a1) + ": "); PrintValues(howSum(7, a1));
+            Console.Write(canSum(7, a2) + ": "); PrintValues(howSum(7, a2));
+            Console.Write(canSum(7, a3) + ": "); PrintValues(howSum(7, a3));
+            Console.Write(canSum(8, a4) + ": "); PrintValues(howSum(8, a4));
+            Console.Write(canSum_memo(300, a5, _memo) + ": "); PrintValues(howSum_memo(300, a5, _memo2));
+        }
+
+        public static void PrintValues(ArrayList myList)
+        {
+            if (myList == null) ;
+            else foreach (int obj in myList)
+                    Console.Write("   {0}", obj);
+            Console.WriteLine();
         }
 
         static bool canSum(int targetSum, int[] set)
@@ -50,6 +63,42 @@ namespace canSum
             }
             _memo.Add(target, false);
             return false;
+        }
+
+        static ArrayList howSum(int target, int[] set)
+        {
+            if (target == 0) return new ArrayList();
+            if (target < 0) return null;
+            foreach (int num in set)
+            {
+                int remainder = target - num;
+                ArrayList result = howSum(remainder, set);
+                if (result != null)
+                {
+                    result.Add(num);
+                    return result;
+                }
+            }
+            return null;
+        }
+
+        static ArrayList howSum_memo(int target, int[] set, Dictionary<int, ArrayList> _memo)
+        {
+            if (_memo.ContainsKey(target)) return _memo[target];
+            if (target < 0) return null;
+            foreach (int num in set)
+            {
+                int remainder = target - num;
+                ArrayList result = howSum_memo(remainder, set, _memo);
+                if (result != null)
+                {
+                    result.Add(num);
+                    _memo.Add(target, result);
+                    return _memo[target];
+                }
+            }
+            _memo[target] = null;
+            return null;
         }
     }
 }
