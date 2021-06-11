1489b8b918bd3dc6f489e674d8567cf22ed259ad
Talha Salman <talha.salman22@gmail.com>
2021-06-11 17:39:46 +0500

    Created Can Sum? function
    The canSum? function checks whether a certain value can be created from sum of values from a set/array in any order and duplication.

---
+++ canSum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace canSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a1 = { 2, 3 };
+            int[] a2 = { 5, 3, 4, 7 };
+            int[] a3 = { 2, 4 };
+            int[] a4 = { 2, 3, 5 };
+            int[] a5 = { 7, 14 };
+            Console.WriteLine(canSum(7, a1));
+            Console.WriteLine(canSum(7, a2));
+            Console.WriteLine(canSum(7, a3));
+            Console.WriteLine(canSum(8, a4));
+            Console.WriteLine(canSum(300, a5));
+        }
+
+        static bool canSum(int targetSum, int[] set)
+        {
+            if (targetSum == 0) return true;
+            if (targetSum < 0) return false;
+            foreach (int num in set)
+            {
+                int remainder = targetSum - num;
+                if (canSum(remainder, set)) return true;
+            }
+            return false;
+        }
+    }
+}
