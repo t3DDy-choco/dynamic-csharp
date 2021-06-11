0ab01cf24ac0d8e34009a311c876e632f37cebe3
Talha Salman <talha.salman22@gmail.com>
2021-06-11 18:00:05 +0500

    Memoized canSum?
    updated the canSum? method and optimized it to use memoization so that now it can calculate really complex things quickly.

--- canSum/Program.cs
+++ canSum/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace canSum
 {
     class Program
     {
+        static Dictionary<int, bool> _memo = new Dictionary<int, bool>() {
+            {0, true}
+        };
+
         static void Main(string[] args)
         {
             int[] a1 = { 2, 3 };
@@ -15,7 +20,7 @@ namespace canSum
             Console.WriteLine(canSum(7, a2));
             Console.WriteLine(canSum(7, a3));
             Console.WriteLine(canSum(8, a4));
-            Console.WriteLine(canSum(300, a5));
+            Console.WriteLine(canSum_memo(300, a5, _memo));
         }
 
         static bool canSum(int targetSum, int[] set)
@@ -27,6 +32,23 @@ namespace canSum
                 int remainder = targetSum - num;
                 if (canSum(remainder, set)) return true;
             }
+            return false;
+        }
+
+        static bool canSum_memo(int target, int[] set, Dictionary<int, bool> _memo)
+        {
+            if (_memo.ContainsKey(target)) return _memo[target];
+            if (target < 0) return false;
+            foreach (int num in set)
+            {
+                int remainder = target - num;
+                if (canSum_memo(remainder, set, _memo))
+                {
+                    _memo.Add(target, true);
+                    return true;
+                }
+            }
+            _memo.Add(target, false);
             return false;
         }
     }
