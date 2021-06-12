4b404738a754d377d1b4ffb5cb03d9ec55079754
Talha Salman <talha.salman22@gmail.com>
2021-06-12 16:01:57 +0500

    Updated BestSum.cs
    Built Functions

--- SumFunc/BestSum.cs
+++ SumFunc/BestSum.cs
@@ -1,17 +1,52 @@
-using System.Collections;
+using System.Collections.Generic;
 
 namespace SumFunc
 {
     public class BestSum
     {
-        public static void bestSum()
+        public static List<int> bestSum(int target, int[] set)
         {
+            if (target == 0) return new List<int>();
+            if (target < 0) return null;
 
+            List<int> bestCombination = null;
+
+            foreach (int num in set)
+            {
+                int remainder = target - num;
+                List<int> result = bestSum(remainder, set);
+                if (result != null)
+                {
+                    result.Add(num);
+                    if (bestCombination ==  null || result.Count < bestCombination.Count) {
+                        bestCombination = new List<int>(result);
+                    }
+                }
+            }
+            return bestCombination;
         }
 
-        public static void bestSum_memo()
+        public static List<int> bestSum_memo(int target, int[] set, Dictionary<int, List<int>> _memo)
         {
+            if (_memo.ContainsKey(target)) return _memo[target];
+            if (target == 0) return new List<int>();
+            if (target < 0) return null;
 
+            List<int> bestCombination = null;
+
+            foreach (int num in set)
+            {
+                int remainder = target - num;
+                List<int> result = bestSum_memo(remainder, set, _memo);
+                if (result != null)
+                {
+                    result.Add(num);
+                    if (bestCombination ==  null) bestCombination = result;
+                    else if (result.Count < bestCombination.Count) bestCombination = result;
+                }
+            }
+            _memo.Add(target, bestCombination);
+            return bestCombination;
         }
     }
 }
--- SumFunc/Program.cs
+++ SumFunc/Program.cs
@@ -10,30 +10,27 @@ namespace SumFunc
             {0, true}
         };
 
-        static Dictionary<int, ArrayList> _memo2 = new Dictionary<int, ArrayList>() {
-            {0, new ArrayList()}
-        };
+        static Dictionary<int, List<int>> _memo2 = new Dictionary<int, List<int>>();
 
         static void Main(string[] args)
         {
-            int[] a1 = { 2, 3 };
-            int[] a2 = { 5, 3, 4, 7 };
-            int[] a3 = { 2, 4 };
-            int[] a4 = { 2, 3, 5 };
-            int[] a5 = { 7, 14 };
+            int[] a1 = { 5, 3, 4, 7 };
+            int[] a2 = { 2, 3, 5 };
+            int[] a3 = { 1, 4, 5 };
+            int[] a4 = { 1, 2, 5, 25 };
             Console.Write(CanSum.canSum(7, a1) + ": ");
-            PrintValues(HowSum.howSum(7, a1));
-            Console.Write(CanSum.canSum(7, a2) + ": ");
-            PrintValues(HowSum.howSum(7, a2));
-            Console.Write(CanSum.canSum(7, a3) + ": ");
-            PrintValues(HowSum.howSum(7, a3));
-            Console.Write(CanSum.canSum(8, a4) + ": ");
-            PrintValues(HowSum.howSum(8, a4));
-            Console.Write(CanSum.canSum_memo(300, a5, _memo) + ": ");
-            PrintValues(HowSum.howSum_memo(300, a5, _memo2));
+            PrintValues(BestSum.bestSum(7, a1));
+            Console.Write(CanSum.canSum(8, a2) + ": ");
+            PrintValues(BestSum.bestSum(8, a2));
+            Console.Write(CanSum.canSum(8, a3) + ": ");
+            PrintValues(BestSum.bestSum(8, a3));
+            // Console.Write(CanSum.canSum(8, a3) + ": ");
+            // PrintValues(BestSum.bestSum_memo(8, a3, _memo2));
+            Console.Write(CanSum.canSum(100, a4) + ": ");
+            PrintValues(BestSum.bestSum_memo(100, a4, _memo2));
         }
 
-        public static void PrintValues(ArrayList myList)
+        public static void PrintValues(List<int> myList)
         {
             if (myList != null)
             {
